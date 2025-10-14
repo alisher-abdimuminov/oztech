5110d2c5917678b621084d9f7f293b956b97d12b
Ali <alisher.abdimuminov.2005@gmail.com>
2025-10-14 13:22:21 +0500

    bug fix

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.dispatch import receiver
 from datetime import date, timedelta
+from django.db import models, transaction
 from django.db.models.signals import post_save
 
 from users.models import User
@@ -20,10 +20,7 @@ QUESTION_TYPE = (
     ("matchable", "Mos keladigan"),
 )
 
-NOTIFCATION_RECEIVER_TYPE = (
-    ("all", "All"),
-    ("specific", "Specific")
-)
+NOTIFCATION_RECEIVER_TYPE = (("all", "All"), ("specific", "Specific"))
 
 PERMISSION_TYPE = (
     ("monthly", "6 oylik"),
@@ -33,20 +30,26 @@ PERMISSION_TYPE = (
 
 class Question(models.Model):
     question = models.TextField(verbose_name="Savol matni")
-    type = models.CharField(max_length=100, choices=QUESTION_TYPE, verbose_name="Savol turi")
-    created = models.DateTimeField(auto_now_add=True,)
+    type = models.CharField(
+        max_length=100, choices=QUESTION_TYPE, verbose_name="Savol turi"
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.question
-    
+
     class Meta:
         verbose_name = "Savol"
         verbose_name_plural = "Savollar"
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Savol")
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, verbose_name="Savol"
+    )
     value_1 = models.TextField(null=True, blank=True, verbose_name="Qiymat 1")
     value_2 = models.TextField(null=True, blank=True, verbose_name="Qiymat 2")
     is_correct = models.BooleanField(default=False, verbose_name="To'g'ri")
@@ -55,7 +58,7 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.value_1
-    
+
     class Meta:
         verbose_name = "Javob"
         verbose_name_plural = "Javoblar"
@@ -63,17 +66,19 @@ class Answer(models.Model):
 
 class Quiz(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nomi")
-    questions = models.ManyToManyField(Question, related_name="quiz_questions", verbose_name="Savollar")
+    questions = models.ManyToManyField(
+        Question, related_name="quiz_questions", verbose_name="Savollar"
+    )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = "Test"
         verbose_name_plural = "Testlar"
-    
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nomi")
@@ -83,14 +88,14 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def courses(self):
         return Course.objects.filter(subject=self).count()
-    
+
     class Meta:
         verbose_name = "Fan"
         verbose_name_plural = "Fanlar"
-    
+
 
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nomi")
@@ -101,33 +106,38 @@ class Course(models.Model):
     price = models.IntegerField(verbose_name="Narxi")
     students = models.ManyToManyField(User, related_name="course_students", blank=True)
     is_public = models.BooleanField(default=False)
-    
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
     def modules(self):
         return Module.objects.filter(course=self)
-    
+
     def count_students(self):
         return self.students.count()
-    
+
     def count_modules(self):
         return Module.objects.filter(course=self).count()
-    
+
     def count_lessons(self):
         return Lesson.objects.filter(module__course=self).count()
-    
+
     def count_quizzes(self) -> int:
         return Test.objects.filter(lesson__module__course=self).count()
-    
+
     def count_resources(self) -> int:
         return Resource.objects.filter(lesson__module__course=self).count()
 
     def length(self):
-        return Lesson.objects.filter(module__course=self).aggregate(models.Sum("duration")).get("duration__sum") or 0
+        return (
+            Lesson.objects.filter(module__course=self)
+            .aggregate(models.Sum("duration"))
+            .get("duration__sum")
+            or 0
+        )
 
     class Meta:
         verbose_name = "Kurs"
@@ -144,15 +154,25 @@ class CourseRating(models.Model):
 
 
 class Module(models.Model):
-    name = models.CharField(max_length=500, verbose_name='Nomi')
+    name = models.CharField(max_length=500, verbose_name="Nomi")
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Kurs")
-    required = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Talab qilinadi")
-    students = models.ManyToManyField(User, related_name="module_students", blank=True, verbose_name="Talabalar")
-    finishers = models.ManyToManyField(User, related_name="module_finishers", blank=True, verbose_name="Bitirganlar")
+    required = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Talab qilinadi",
+    )
+    students = models.ManyToManyField(
+        User, related_name="module_students", blank=True, verbose_name="Talabalar"
+    )
+    finishers = models.ManyToManyField(
+        User, related_name="module_finishers", blank=True, verbose_name="Bitirganlar"
+    )
 
     def __str__(self) -> str:
         return self.name
-    
+
     def finished_lessons(self, user: User):
         lessons = Lesson.objects.filter(module=self)
         count = 0
@@ -160,24 +180,29 @@ class Module(models.Model):
             if user in lesson.finishers.all():
                 count += 1
         return count
-    
+
     def count_students(self) -> int:
         return self.students.count()
-    
+
     def count_finishers(self) -> int:
         return self.finishers.count()
-    
+
     def count_lessons(self) -> int:
         return Lesson.objects.filter(module=self).count()
-    
+
     def count_quizzes(self) -> int:
         return Lesson.objects.filter(module=self, type="quiz").count()
-    
+
     def lessons(self):
         return Lesson.objects.filter(module=self)
-    
+
     def video_length(self) -> int:
-        return Lesson.objects.filter(module=self).aggregate(duration=models.Sum("duration")).get("duration") or 0
+        return (
+            Lesson.objects.filter(module=self)
+            .aggregate(duration=models.Sum("duration"))
+            .get("duration")
+            or 0
+        )
 
     class Meta:
         verbose_name = "Modul"
@@ -189,31 +214,47 @@ class Lesson(models.Model):
     module = models.ForeignKey(Module, on_delete=models.CASCADE, verbose_name="Modul")
     type = models.CharField(max_length=100, choices=LESSON_TYPE, verbose_name="Turi")
     duration = models.IntegerField(default=60, verbose_name="Davomiyligi")
-    previous = models.ForeignKey("self", related_name="previous_lesson", on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Oldingi dars")
-    next = models.ForeignKey("self", related_name="next_lesson", on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Keyingi dars")
-    finishers = models.ManyToManyField(User, related_name="lesson_finishers", blank=True, verbose_name="Tugatganlar")
+    previous = models.ForeignKey(
+        "self",
+        related_name="previous_lesson",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Oldingi dars",
+    )
+    next = models.ForeignKey(
+        "self",
+        related_name="next_lesson",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Keyingi dars",
+    )
+    finishers = models.ManyToManyField(
+        User, related_name="lesson_finishers", blank=True, verbose_name="Tugatganlar"
+    )
     is_public = models.BooleanField(default=False, verbose_name="Ochiq dars")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
     def is_quiz(self):
         return True if self.quiz else False
-    
+
     def has_previous(self):
         return True if self.previous else False
-    
+
     def has_next(self):
         return True if self.next else False
-    
+
     def count_finishers(self):
         return self.finishers.count()
-    
+
     def finishers_list(self):
         return self.finishers.all()
-    
+
     def end_lesson(self, user: User):
         self.finishers.add(user)
 
@@ -225,11 +266,16 @@ class Lesson(models.Model):
 class Test(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    url = models.FileField(upload_to="files/lessons/tests", null=True, blank=True, verbose_name="Test manbaa")
+    url = models.FileField(
+        upload_to="files/lessons/tests",
+        null=True,
+        blank=True,
+        verbose_name="Test manbaa",
+    )
 
     def __str__(self):
         return self.lesson.name
-    
+
     class Meta:
         verbose_name = "Test manba"
         verbose_name_plural = "Test manbalar"
@@ -242,20 +288,22 @@ class Video(models.Model):
 
     def __str__(self):
         return self.lesson.name
-    
+
     class Meta:
         verbose_name = "Video"
         verbose_name_plural = "Videolar"
-    
+
 
 class Resource(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    url = models.FileField(upload_to="files/lessons", null=True, blank=True, verbose_name="Manbaa")
+    url = models.FileField(
+        upload_to="files/lessons", null=True, blank=True, verbose_name="Manbaa"
+    )
 
     def __str__(self):
         return self.lesson.name
-    
+
     class Meta:
         verbose_name = "Manba"
         verbose_name_plural = "Manbalar"
@@ -284,7 +332,7 @@ class Permission(models.Model):
 
     def __str__(self):
         return self.type
-    
+
     def save(self, *args, **kwargs):
         if self.type == "monthly":
             self.ended = date.today() + timedelta(days=182)
@@ -306,13 +354,17 @@ class Notification(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     type = models.CharField(max_length=100)
-    receivers = models.ManyToManyField(User, related_name="notification_receivers", blank=True)
-    readers = models.ManyToManyField(User, related_name="notification_readers", blank=True)
+    receivers = models.ManyToManyField(
+        User, related_name="notification_receivers", blank=True
+    )
+    readers = models.ManyToManyField(
+        User, related_name="notification_readers", blank=True
+    )
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
 
 class Banner(models.Model):
     title = models.CharField(max_length=100)
@@ -327,12 +379,17 @@ class Banner(models.Model):
 @receiver(post_save, sender=Course)
 def notify_new_course(sender, instance: Course, created, **kwargs):
     if created:
-        users = User.objects.all()
-        notification = Notification.objects.create(
-            title="Yangi kurs qo'shildi",
-            description=f"{instance.name} - {instance.user.full_name}",
-            type="new_course",
-            receivers=users
-        )
-        worker = Worker(notify, notification=notification, users=users)
-        worker.start()
+        return
+    users = User.objects.all()
+    user_ids = list(User.objects.values_list("id", flat=True))
+    notification = Notification.objects.create(
+        title="Yangi kurs qo'shildi",
+        description=f"{instance.name} - {instance.user.full_name}",
+        type="new_course",
+    )
+    notification.receivers.set(user_ids)
+
+    worker = Worker(notify, notification=notification, users=users)
+    worker.start()
+
+    transaction.on_commit()
